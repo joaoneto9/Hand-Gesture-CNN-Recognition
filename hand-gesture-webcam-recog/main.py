e5b63791e71abd1bbd829e5c9c49471037249810
joaoneto9 <joaonetocg1@gmail.com>
2026-07-30 22:12:18 -0300

    feat: adding the menu to choose the CNN model for inferance

--- hand-gesture-webcam-recog/main.py
+++ hand-gesture-webcam-recog/main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from hand-gesture-webcam-recog!")
+from pathlib import Path
+from typing import final
 
 
-if __name__ == "__main__":
-    main()
+neural_network_map = {
+    "1": "baseline",
+    "2": "transfer-learning",
+    "3": "fine-tuning"
+}
+
+def init_app() -> str:
+    input_text = """
+    Escolha uma das redes para usar a web-cam:
+
+    1) baseline
+    2) transfer learning
+    3) fine-tuning
+
+    --- 
+    """
+
+    neural_network_map = {
+        "1": "baseline",
+        "2": "transfer-learning",
+        "3": "fine-tuning"
+    }
+
+    print(input_text)
+    choice = input("Escolha: ")
+
+    if not neural_network_map.get(choice):
+        print("Escolha uma Opção válida.")
+        init_app()
+
+    return neural_network_map[choice]
+
+
+final_path: str = init_app()
+
+path = Path(f"neural-networks/{final_path}")
